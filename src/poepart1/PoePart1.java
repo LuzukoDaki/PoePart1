07f62c9beec4b8620664d8a06f28a062f168583e
LuzukoDaki <lzkdaki@gmail.com>
2024-05-15 14:01:37 +0200

    Login system which includes input and password methods Corrected the calling of methods Added comments Added more messages to be displayed to user

--- src/poepart1/PoePart1.java
+++ src/poepart1/PoePart1.java
@@ -18,53 +18,68 @@ public class PoePart1 {
        
     // creating class to get the methods from the other class
    public static void getuserInput(){
-    //method to request user
-    login.getName();
-    login.getLastName();
-    // assigning values to attributes
-    // we will login attributes they are public
+    //method to request user input
+    login.setName(JOptionPane.showInputDialog("Enter your Name "));
+    login.setLastName(JOptionPane.showInputDialog("Enter your last Name "));
+    
+    
+    
+    // Using the set method to diplay conditions the user must input
     login.setuserName(JOptionPane.showInputDialog("Enter your user name which must contain an _ and must be no more than 5 characters long "));
-    login.getUserName();
-    boolean uCheck = login.checkUserName();
-    if(uCheck == false ){
-        JOptionPane.showMessageDialog(null, "User Name is not correctly formated, please  ensure correct instruction is followed ");
-        }
-    if(uCheck == true){
-        JOptionPane.showMessageDialog(null, "user name is correctly formatted ");
-        }
-    login.setpassWord(JOptionPane.showInputDialog("Enter your password which must be at least 8 characters long. It must contain a capital letter, a number, and a special character "));
-    login.getPassword();
-    boolean pCheck = login.checkPasswordComplexity();
-    if(pCheck == false){
-        JOptionPane.showMessageDialog(null, "Password is not correctly formated, please  ensure correct instruction is followed ");
-        }
-       if(pCheck == true){
-        JOptionPane.showMessageDialog(null, "Password succesfully captured "); 
-        }
-       login.setName(JOptionPane.showInputDialog("Please enter first name "));
-       login.getName();
-       login.setLastName(JOptionPane.showInputDialog("Enter your last name "));
-       login.getLastName();
-       if(uCheck == true){
-        JOptionPane.showMessageDialog(null, "Welcom " + login.getName() + " " + login.getLastName() +  " it is great to see you ");
-        }
-        
-       
-    String loginUserName = "";
-    String loginPassWord = "";
-    boolean logCheck = login.loginUser(loginUserName, loginPassWord);
-  
+    
+    // Using the set method to diplay conditions the user must input
+    login.setpassWord(JOptionPane.showInputDialog("Enter your password which must be at least 8 characters long. It must contain a capital letter, a number, and a special character "));  
+    
    }
-
+  
     public static void main(String[] args) {
-        // calling getUser method for 
-        getuserInput();
+       // Creating a method for user input
+       getuserInput();
+       // Calling the registerUser method from the login class
+       String registerMessage = login.registerUser();
+       // Dispalying message set in the register method
+       JOptionPane.showMessageDialog(null,registerMessage);
+       
+       // Using the while loop to dipaly method
+       while (!registerMessage.endsWith("Username and Password have been successfully captured")){
+           login.setuserName(JOptionPane.showInputDialog("Enter your user name which must contain an _ and must be no more than 5 characters long "));
+           login.setpassWord(JOptionPane.showInputDialog("Enter your password which must be at least 8 characters long. It must contain a capital letter, a number, and a special character "));
+           
+           registerMessage = login.registerUser();
+           JOptionPane.showMessageDialog(null,registerMessage);   
+       }       
+       // Calling matchLogin method
+       matchLogin();
+    }
+    // Creating this method to vefrify the user deatil
+    public static void matchLogin(){
+        String loginUserName, loginPassWord;
+        loginUserName = JOptionPane.showInputDialog(null,"Re-enter your user name ");
+        loginPassWord = JOptionPane.showInputDialog(null, "Re-enter your password ");
+        
+        boolean isUserLogin = false;
+        // Using while loop to loop the method if condition is not met
+        while (!isUserLogin){
+            // Check if the entered login match the stored credentials
+            isUserLogin = login.loginUser(loginUserName, loginPassWord);
+            // Using if statement to display message if user input is incorrect
+            if(!isUserLogin){
+                JOptionPane.showMessageDialog(null,"Username or password incorrect, please try agin ");
+                loginUserName = JOptionPane.showInputDialog(null,"Enter your user name ");
+                loginPassWord = JOptionPane.showInputDialog(null, "Enter your password ");
+            }
+            
+        }
+        
+        String status = login.returnLoginStatus(isUserLogin);
+        
+        //Dispaly the welcome message
+        JOptionPane.showMessageDialog(null, status);
         
         
-   
+                
     }
     
-    
-    
 }
+
 
